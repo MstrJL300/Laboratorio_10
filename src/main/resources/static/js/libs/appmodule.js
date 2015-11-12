f264158d3e12c0aecfddee640c96e48fc660e502
MstrJL300 <joslum3@icloud.com>
2015-11-12 12:29:55 -0500

    1er commit PC: ECI Ubuntu

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -5,8 +5,26 @@
         app.controller('plan_control', 
 
             function($scope,$http){
+                $scope.posts = [];
+                $scope.loadData = function() {
+                    var configList = {
+                        method: "GET",
+                        url: "blueprints"
+                    };
 
+                    var response=$http(configList);
+
+                    response.success(function(data, status, headers, config) {
+                        $scope.variable = data;
+                    });
+
+                    response.error(function(data, status, headers, config) {
+                        alert("The petition has failed. HTTP Status:"+status);
+                    });
+                };
             }
+                    
+            
         );
     }        
 )
