33c23e7fd02c8a4c8adf21a95ad16e1a6ea50d9f
MstrJL300 <joslum3@icloud.com>
2015-11-26 09:58:51 -0500

    avance en js

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -2,9 +2,8 @@
     function () {
         var app = angular.module('modone', []);
 
-        app.controller('plan_control', 
-
-            function($scope,$http){
+        app.controller('plan_control', function($scope,$http){
+                
                 $scope.posts = [];
                 
                 $scope.loadData = function() {
@@ -16,17 +15,14 @@
                     var response=$http(configList);
 
                     response.success(function(data, status, headers, config) {
-                        $scope.post = data;
+                        $scope.posts = data;
                     });
 
                     response.error(function(data, status, headers, config) {
                         alert("The petition has failed. HTTP Status:"+status);
                     });
                 };
-            }
-                    
-            
-        );
+        });
     }        
 )
 ();
